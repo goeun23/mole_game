d86af92508d239acbcaa20b3b3044079fff4a3b9
allen.cge <allen.cge@kakaocorp.com>
2022-02-20 19:26:28 +0900

    라우터 세팅/행, 열, 두더지 입력값 페이지 세팅

---
+++ src/views/ready.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div>행과 열은 최대 2에서 6까지 입력 가능합니다.</div>
+
+    <div>행 : <input type="number" v-model="row" /></div>
+    <div>열 : <input type="number" v-model="col" /></div>
+    <div v-if="col && row">
+      두더지는 최소 1에서 {{ countofMole }} 까지 입력 가능합니다.
+      <div>두더지 : <input type="number" v-model="mole" /></div>
+    </div>
+
+    <div>
+      <button @click="startGame">시작</button>
+      <div>{{ errorMsg }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      col: 0,
+      row: 0,
+      mole: 0,
+      errorMsg: "",
+    };
+  },
+  computed: {
+    countofMole() {
+      if (this.col > 0 && this.row > 0) {
+        return parseInt((this.col * this.row) / 2);
+      } else {
+        return 1;
+      }
+    },
+  },
+
+  methods: {
+    dataInvalidCheck() {
+      const { col, row, mole, countofMole } = this;
+
+      // 행, 열, 두더지 하나라도 입력되지 않은 경우 > 오류
+      if (col === 0 || row === 0 || mole === 0) {
+        this.errorMsg = "입력값을 모두 입력해주세요!";
+        return false;
+      }
+
+      // 행과 열의 값이 유효한지 확인
+      if (col < 2 || col > 6 || row < 2 || row > 6) {
+        this.errorMsg = "행과 열은 최대 2에서 6까지 입력 가능합니다.";
+        return false;
+      }
+
+      // 두더지의 입력값이 유효한지 확인
+      if (mole < 1 || mole > countofMole) {
+        this.errorMsg = `두더지 입력값을 확인해주세요.`;
+        return false;
+      }
+
+      return true;
+    },
+    startGame() {
+      if (this.dataInvalidCheck()) this.$router.push("/game");
+    },
+  },
+  mounted() {},
+};
+</script>
